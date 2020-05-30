2f323745e0816b9fc666f124da5fad7e3a318043
jp_darkarmour <jpchennai462@gmail.com>
2020-05-30 19:09:05 +0530

    updates

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0a06535bf073452ee8b2";
+/******/ 	var hotCurrentHash = "d3399c2ef248d17c8259";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
