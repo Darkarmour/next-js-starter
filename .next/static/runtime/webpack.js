7f39a73d7cdeff6e7b057b269b809f342f88330b
jp_darkarmour <jpchennai462@gmail.com>
2020-05-31 19:02:28 +0530

    dynamic routes updates

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5d9aeaec7878c5a162ae";
+/******/ 	var hotCurrentHash = "3d18981c0f40699a79e7";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
